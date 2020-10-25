d064c53f33bb59ccde50a9ad4c41e937ac35a923
gabriellydeandrade <gabrielly.andrade@poli.ufrj.br>
2020-10-24 21:56:50 -0300

    refactor: separating functionalities into another js file and removing unnecessary script link

--- case-dp6/js/tagueamento.js
+++ case-dp6/js/tagueamento.js
@@ -16,23 +16,8 @@ function handleEventClick(category, action, label) {
         });
 }
 
-// Link menu
-
 var contactEvent = document.getElementById('contato');
 contactEvent.addEventListener('click', function(){handleEventClick('menu', 'entre_em_contato', 'link_externo')});
 
 var downloadPDF = document.getElementById('download-pdf');
 downloadPDF.addEventListener('click', function(){handleEventClick('menu', 'download_pdf', 'download_pdf')});
-
-// Analise page
-
-var analysisIpsum = document.getElementById('analise-ipsum');
-analysisIpsum.addEventListener('click', function(){handleEventClick('analise', 'ver_mais', analysisIpsum.getAttribute('data-name'))});
-
-
-var analysisDolor = document.getElementById('analise-dolor');
-analysisDolor.addEventListener('click', function(){handleEventClick('analise', 'ver_mais', analysisDolor.getAttribute('data-name'))});
-
-
-var analysisLorem = document.getElementById('analise-lorem');
-analysisLorem.addEventListener('click', function(){handleEventClick('analise', 'ver_mais', analysisLorem.getAttribute('data-name'))});
---
+++ case-dp6/js/tagueamento_analise.js
@@ -0,0 +1,10 @@
+var analysisIpsum = document.getElementById('analise-ipsum');
+analysisIpsum.addEventListener('click', function(){handleEventClick('analise', 'ver_mais', analysisIpsum.getAttribute('data-name'))});
+
+
+var analysisDolor = document.getElementById('analise-dolor');
+analysisDolor.addEventListener('click', function(){handleEventClick('analise', 'ver_mais', analysisDolor.getAttribute('data-name'))});
+
+
+var analysisLorem = document.getElementById('analise-lorem');
+analysisLorem.addEventListener('click', function(){handleEventClick('analise', 'ver_mais', analysisLorem.getAttribute('data-name'))});
