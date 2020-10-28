cc97ea2d3ece4275a00c4f341f66f4f85f7cb426
gabriellydeandrade <gabrielly.andrade@poli.ufrj.br>
2020-10-28 09:27:52 -0300

    refactor: using querySelector instead of id for analysis click event

--- case-dp6/js/tagueamento_analise.js
+++ case-dp6/js/tagueamento_analise.js
@@ -1,10 +1,8 @@
-var analysisIpsum = document.getElementById('analise-ipsum');
-analysisIpsum.addEventListener('click', function(){handleEvent('analise', 'ver_mais', analysisIpsum.getAttribute('data-name'))});
+var analysis = document.querySelectorAll('.card-montadoras');
 
+analysis.forEach(addEvent);
 
-var analysisDolor = document.getElementById('analise-dolor');
-analysisDolor.addEventListener('click', function(){handleEvent('analise', 'ver_mais', analysisDolor.getAttribute('data-name'))});
+function addEvent(analysisElement){
+    analysisElement.addEventListener('click', function(){handleEvent('analise', 'ver_mais', analysisElement.getAttribute('data-name'))});
 
-
-var analysisLorem = document.getElementById('analise-lorem');
-analysisLorem.addEventListener('click', function(){handleEvent('analise', 'ver_mais', analysisLorem.getAttribute('data-name'))});
+}
